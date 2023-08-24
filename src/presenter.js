b21333dfa93d2ea760791bf3d33789b6774e00b8
Alejandro Montero Coca <alejandro.montero@ucb.edu.bo>
2023-08-24 09:38:55 -0400

    Funcionalidad seleccionar estado y mostrarlo

--- src/presenter.js
+++ src/presenter.js
@@ -4,13 +4,14 @@ const cantidad = document.querySelector("#cantidad");
 const precio = document.querySelector("#precio")
 const form = document.querySelector("#ventas-form");
 const div = document.querySelector("#resultado-div");
+const estado = document.querySelector("#estados");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const cantidadItem = Number.parseInt(cantidad.value);
   const precioItem = Number.parseInt(precio.value)
-
-  div.innerHTML = "<p>" + cantidad.value + "</p> <p>" + precio.value + "</p>";
+  const estadoSeleccionado = estado.value;
+  div.innerHTML = "<p>" + cantidad.value + "</p> <p>" + precio.value + "</p> <p>" + estadoSeleccionado + "</p>" ;
 });
 
 
