938b7393259de3692b6745f42ce0b3032097e8be
Alejandro Montero Coca <alejandro.montero@ucb.edu.bo>
2023-08-28 00:06:13 -0400

    funcionalidad mostrar el precio total con impuesto corregida para TX y CA

--- src/presenter.js
+++ src/presenter.js
@@ -13,7 +13,8 @@ form.addEventListener("submit", (event) => {
   const estadoSeleccionado = estado.value;
   const SP1 = new SellPoint(estadoSeleccionado, cantidadItem, precioItem);
   div.innerHTML = "<p>" + cantidad.value + "</p> <p>" + precio.value + "</p> <p>" + estadoSeleccionado + "</p>" 
-  +"<p>" + SP1.getPorcentaje() + "</p> <p>" + SP1.getPrecioNeto() + "</p>";
+  +"<p>" + SP1.getPorcentaje() + "</p> <p>" + "Precio Neto es:"+ SP1.getPrecioNeto() + "</p> <p>" + 
+      "Precio con imouesto es: " + SP1.getTotalConImpuesto();
 });
 
 
